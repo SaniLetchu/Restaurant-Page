17ae9ed555171d8fcc585be7febc225b855fe15e
Sani Letchu <sani.letchu@gmail.com>
2022-05-31 11:23:06 +0300

    Initial files complete, ready to finally code maybe :=)

---
+++ src/index.js
@@ -0,0 +1,30 @@
+//Import the content from style.css
+import './style.css';
+
+//Import all the different content for the site
+import contactContent from "./Contact";
+import landingContent from "./Landing-page";
+import menuContent from "./Contact";
+
+
+//Creates the header for the site
+function createHeader() {
+
+}
+
+//Creates the content in the #content div
+function createContent(content) {
+    divcontent.replaceChildren(content);
+}
+
+//Creates the footer for the site
+function createFooter() {
+    
+}
+//Create the div for content
+const divcontent = document.createElement("div");
+divcontent.setAttribute("id", "content");
+
+
+
+document.body.appendChild(divcontent);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,48 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
   },
 };
